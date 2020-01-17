900a6e372094ab3cfa55a0aa284e865fc5a7d0a2
Victoria Argon <vkkizilova@gmail.com>
2020-01-17 20:06:34 +0300

    LogLevel and test are added

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,61 @@
 package apiserver
 
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+)
+
 //APIServer
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
+	router *mux.Router
 }
 
 //New
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
 //Start
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+
+	s.logger.Info("starting api server")
+
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handelHello())
+}
+
+func (s *APIServer) handelHello() http.HandlerFunc {
+	type request struct {
+		name string
+	}
+
+	return func(w http.ResponseWriter, request *http.Request) {
+		io.WriteString(w, "Hello")
+	}
 }
